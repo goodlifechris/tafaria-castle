1c5b6611bbe211c5aba4f10b94174df6de8306b9
goodlifechris <christopherndugo@gmail.com>
2025-01-27 22:50:04 +0400

    set up url

--- graphql-client.js
+++ graphql-client.js
@@ -2,8 +2,9 @@
 import { GraphQLClient } from 'graphql-request';
 
 const graphqlClient = new GraphQLClient(
-    'https://www.tafaria.com/api/graphql',
-    {
+  'http://209.38.189.197:3003/api/graphql',
+  // 'https://www.tafaria.com/api/graphql',
+  {
       headers: {
         'Content-Type': 'application/json', // Ensure the correct content type
         Authorization: `Bearer YOUR_ACCESS_TOKEN`, // Replace with your actual token
