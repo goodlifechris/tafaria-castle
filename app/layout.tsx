2ab181d7460afe38fce505e9a62cf4f7a177b33e
goodlifechris <christopherndugo@gmail.com>
2025-04-03 22:52:35 +0400

    update css bookings

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import "./booknow.css";
 import Head from "next/head";
 import Header from "./components/header";
 import Stories from "./components/stories";
