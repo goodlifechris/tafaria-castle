ebf27bb1a6ef00999f8c289f590a96f654341921
goodlifechris <christopherndugo@gmail.com>
2025-04-03 23:57:34 +0400

    update css

--- app/book/page.tsx
+++ app/book/page.tsx
@@ -2,15 +2,22 @@
 
 import React from "react";
 import BookNow from "../components/booknow";
+import Head from "next/head";
 
 
 const Book = () => {
 
     return (
+      <>
+            <Head>
+      <link rel="stylesheet" href="https://nebulacrs.hti.app/hostech/booknow/static/css/booknow.css" />
+    </Head>
       <div className="flex justify-center items-center min-h-screen">
         <p className="text-2xl font-bold mb-4">Book Now</p>
              <BookNow />
       </div>
+      </>
+
     );
   }
 ;
