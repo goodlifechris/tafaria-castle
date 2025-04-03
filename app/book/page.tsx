31d39f9a58effa40fafe726a9394877b7948bc94
goodlifechris <christopherndugo@gmail.com>
2025-04-03 23:45:09 +0400

    update css

---
+++ app/book/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import React from "react";
+import BookNow from "../components/booknow";
+
+
+const Book = () => {
+
+    return (
+      <div className="flex justify-center items-center min-h-screen">
+        <p className="text-2xl font-bold mb-4">Book Now</p>
+             <BookNow />
+      </div>
+    );
+  }
+;
+
+
+export default Book;
